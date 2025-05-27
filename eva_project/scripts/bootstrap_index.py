1f8b9f515b0e6450d47bcdcb9459483d6f80e57a
Joey LoSurdo <joeylosurdo@gmail.com>
2025-05-27 14:23:10 -0500

    update

--- eva_project/scripts/bootstrap_index.py
+++ eva_project/scripts/bootstrap_index.py
@@ -4,6 +4,8 @@ from backend.ingest.extract_text import extract_text_from_file
 from backend.ingest.chunker import chunk_text
 from backend.ingest.embedder import upsert_chunks
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def run_index():
     files = list_drive_files()
